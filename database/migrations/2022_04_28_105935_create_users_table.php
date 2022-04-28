6b8c34f8b8bfec677a10a0a43dc04d53a322639f
HaidarJb <Abdularoufhasan@gmail.com>
2022-04-28 14:37:51 +0300

    Build (DB, Models): Adding the migration and Models files for Users and Images tables

--- database/migrations/2022_04_28_105935_create_users_table.php
+++ database/migrations/2022_04_28_105935_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Image;
 
 class CreateUsersTable extends Migration
 {
@@ -15,12 +16,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('username');
+            $table->integer('karma_score')->unsigned()->default(0);
+            $table->foreignIdFor(Image::class,'image_id')->nullable()->references('id')->on('images')->onDelete('cascade');
         });
     }
 
